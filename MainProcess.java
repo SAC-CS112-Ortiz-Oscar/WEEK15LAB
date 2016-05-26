12832252a182052f9c7f2d39a0681b4f8f9f988a
SAC-CS112-Ortiz-Oscar <oo17570@student.sac.edu>
2016-05-25 21:10:08 -0700

    Using Executor

--- MainProcess.java
+++ MainProcess.java
@@ -7,19 +7,15 @@ public class MainProcess
 		System.out.println("Start");
 		for (int i = 0; i < 25; i++) 
 		{
-		      Runnable T1 = new Dice(10000L + i);
-		      Thread roll = new Thread(T1);
-		      // We can set the name of the thread
-		      roll.setName(String.valueOf(100));
-		      // Start the thread, never call method run() direct
-		      roll.start();
-	      
+		     Runnable T1 = new Dice(10000L + i); 
 		     Runnable T2 = new Dice(10000L + 2*i);
-		      Thread roll2 = new Thread(T2);
-		      // We can set the name of the thread
-		      roll2.setName(String.valueOf(100));
-		      // Start the thread, never call method run() direct
-		      roll2.start();
+		     
+		     System.out.println("Starting Executor!");
+		     
+		     ExecutorService threadExcecutor = Executors.newCachedThreadPool();
+		     
+		     threadExcecutor.execute(T1);
+		     threadExcecutor.execute(T2);
 		}
 	    System.out.println("Complete");
 	 }
